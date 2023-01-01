79aa6a63db3be4d1009e5162d09e753b78ee7d9f
Sowmesh <sowmeshshailesh1234@gmail.com>
2023-01-01 17:12:01 +0530

    ReadFromAPI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_notion_budget/budget_repository.dart';
@@ -5,6 +6,8 @@ import 'package:flutter_notion_budget/failure_model.dart';
 import 'package:flutter_notion_budget/item_model.dart';
 import 'package:flutter_notion_budget/spending_chart.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -42,11 +45,52 @@ class _BudgetScreenState extends State<BudgetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Link(
+          target: LinkTarget.blank,
+          uri: Uri.parse(
+              'https://www.notion.so/903d8194cf934ac7a8522a4231db3119?v=2094f281651f41d4933ef882a8e6c37a'),
+          builder: (context, followLink) {
+            return ElevatedButton(
+                onPressed: followLink,
+                child: Text(
+                  'Edit Your Data',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ));
+          },
+        ),
+      ),
+      /*bottomNavigationBar: Container(
+        child: RichText(
+            text: TextSpan(children: [
+          TextSpan(
+              text: 'Edit Your Database',
+              style: new TextStyle(color: Colors.white),
+              recognizer: new TapGestureRecognizer()
+                ..onTap = () {
+                  Link(
+                      uri: Uri.parse(
+                          'https://www.notion.so/903d8194cf934ac7a8522a4231db3119?v=2094f281651f41d4933ef882a8e6c37a'),
+                      target: LinkTarget.blank,
+                      builder: (context, followLink) {
+                        return ElevatedButton(
+                            onPressed: followLink,
+                            child: Text('Edit Your Database'));
+                      });
+                }),
+        ])),
+        height: 60,
+        decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      ),*/
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Budget Tracker'),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
       body: RefreshIndicator(
         onRefresh: () async {
@@ -103,6 +147,11 @@ class _BudgetScreenState extends State<BudgetScreen> {
             return const Center(child: CircularProgressIndicator());
           },
         ),
+        /*Icon(
+              Icons.add,
+              size: 30,
+              color: Colors.white,
+            ),*/
       ),
     );
   }
